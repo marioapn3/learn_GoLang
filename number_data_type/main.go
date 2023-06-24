dfe7328c216b4a36ae31843ade81574e174afbae
marioapn3 <mario.aprilnino27@gmail.com>
2023-06-24 09:06:56 +0700

    number data type

---
+++ number_data_type/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func main(){
+	/*Number data type
+		int8 = -128 - 127
+		int16 = -32768 - 32767
+		int32 = -2147483648 - 2147483647
+		int64 = -9223372036854775808 - 9223372036854775807
+
+		uint no no minimun = 0
+			uint8 = 255
+			uint16 = 65535
+			uint32 = 4292967295
+			uint64 = many
+
+		floating point
+			float32 , float64,complex64,complex128
+
+
+		Alias (nama lain)
+			byte = uint8
+			rune = int32
+			int = minimal int32
+			uint = minimal uint32
+	*/
+	fmt.Println("Satu = " , 1)
+	fmt.Println("Dua = " , 2)
+	fmt.Println("Tiga koma lima = " , 3.5)
+	fmt.Println( 3.5 + 2)
+}
